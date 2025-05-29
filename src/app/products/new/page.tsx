5f877ff242996cbca25d859aee1b0f2affd0d939
jinew <jiyunk@naver.com>
2025-05-29 12:03:53 +0900

    setting:초기 세팅

---
+++ src/app/products/new/page.tsx
@@ -0,0 +1,5 @@
+const AddProductPage = () => {
+  return <div>상품 등록</div>;
+};
+
+export default AddProductPage;
---
+++ src/app/products/page.tsx
@@ -0,0 +1,5 @@
+const ProductListPage = () => {
+  return <div>상품 목록</div>;
+};
+
+export default ProductListPage;
