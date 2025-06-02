3d6776e2593b86b8594580456d835a7fd838adab
jinew <jiyunk@naver.com>
2025-06-02 13:57:19 +0900

    refactor: Next image 사용

--- src/app/components/ProductCard.tsx
+++ src/app/components/ProductCard.tsx
@@ -1,9 +1,15 @@
 import { Product } from '@/types/product';
+import Image from 'next/image';
 
 const ProductCard = ({ product }: { product: Product }) => {
   return (
     <article className="p-4 border-2 border-gray-300 rounded-lg space-y-2">
-      <img src={product.thumbnail} alt={product.title} />
+      <Image
+        src={product.thumbnail}
+        alt={product.title}
+        width={300}
+        height={300}
+      />
       <h3>상품명 : {product.title}</h3>
       <div className="space-y-2">
         <p>상품 설명 : {product.description}</p>
